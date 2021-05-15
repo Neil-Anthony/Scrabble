fe8dd0a4a45f6d6cf8aa75c504384c00f18cec36
Neil-Anthony <neil.anthony.email@gmail.com>
2021-05-14 18:28:42 -0700

    Check if word fits methods.

--- src/Scrabble.java
+++ src/Scrabble.java
@@ -1,35 +1,94 @@
 // How to handle scoring?
 // GUI (array backed grid with python?)
+// No diagonal or backwards words. Left to right, top to bottom.
 
+
+import java.util.Scanner;
 
 public class Scrabble {
-    public static void main(String[] args) {
-        char [][] scrabble_Board = new char[14][14];
-        scrabble_Board[1][3] = 'X'
-        printGrid(scrabble_Board)
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        char[][] scrabble_Board = new char[14][14];
+        insertWord(scrabble_Board);
+        printBoard(scrabble_Board);
     }
 
     //Asks what word was played, and where it was played.
     //Checks if the word is legal.
     //Asks if you challenge.
     protected static char[][] update_Board(){
-
+    return null;
     }
 
     protected static void check_Vertical(){
 
     }
 
-    public void printGrid(char[][] scrabble_Board)
-    {
-        for(int i = 0; i < 14; i++)
-        {
-            for(int j = 0; j < 14; j++)
-            {
-                System.out.printf("%5d ", scrabble_Board[i][j]);
+    public static void printBoard(char[][] board){
+        // Displays a two dimensional array.
+        for (char[] letter: board) {
+            for (char element: letter) {
+                System.out.print(element + " ");
             }
             System.out.println();
         }
+    }
+
+    public static void insertWord(char[][] board)
+    {
+        int x_Cordinate = 4;
+        int y_Coordinate = 7;
+        String word = input.next();
+        int word_Length = word.length();
+        if (word.length() < (14 - y_Coordinate))
+        {
+            for (int y = 0; y<word_Length; y++)
+            {
+                board[y+y_Coordinate][x_Cordinate] = word.charAt(y);
+            }
+        }
+    }
+
+    public static boolean checkVertical(char[][] board, String word)
+    {
+        int x_Coordinate = 3;
+        int y_Coordinate = 4;
+        int w_Length = word.length();
+        if (y_Coordinate+w_Length>13)
+        {
+            return false;
+        }
+
+        for (int y = 0; y < w_Length; y++)
+        {
+            if(board[y+y_Coordinate][x_Coordinate]!=(word.charAt(y))&&(board[y+y_Coordinate][x_Coordinate])!='\u0000')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkHorizontal(char[][] board, String word)
+    {
+        int x_Coordinate = 3;
+        int y_Coordinate = 4;
+        int w_Length = word.length();
+        if (x_Coordinate+w_Length>13)
+        {
+            return false;
+        }
+
+        for (int x = 0; x < w_Length; x++)
+        {
+            if(board[y_Coordinate][x_Coordinate+x]!=(word.charAt(x))&&(board[y_Coordinate][x_Coordinate+x])!='\u0000')
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
 
